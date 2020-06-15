5683e861a43e7cdbf8f96c576b2b4f646618e564
DesantBucie <mattrusz@gmail.com>
2020-06-15 14:56:49 +0200

    Added website, database not working

---
+++ docs/precache-manifest.ec5c86375da82076886e9f14c9263b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb09f3eab1e0af1574c309e57cac3a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7686d80d71f1cc582124",
+    "url": "/static/css/2.d805e148.chunk.css"
+  },
+  {
+    "revision": "b3c4b2d13a23bf5503ca",
+    "url": "/static/css/main.e541e02e.chunk.css"
+  },
+  {
+    "revision": "7686d80d71f1cc582124",
+    "url": "/static/js/2.11d2220b.chunk.js"
+  },
+  {
+    "revision": "0493177a5bc1903429231130df2d56cd",
+    "url": "/static/js/2.11d2220b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c4b2d13a23bf5503ca",
+    "url": "/static/js/main.b5713f35.chunk.js"
+  },
+  {
+    "revision": "578daf81d163e1be5f0d",
+    "url": "/static/js/runtime-main.1cf8bf64.js"
+  }
+]);
