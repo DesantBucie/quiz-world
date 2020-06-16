49ada8f4397040da04f7f21ca0fdeb65b65fb3d1
qalvik <46355180+qalvik@users.noreply.github.com>
2020-06-16 12:50:29 +0200

    Revert "Revert "FIxing""
    This reverts commit 2f9fbc2897b223c9ad667d7eaf6add6c5e80bbce.

--- docs/precache-manifest.1751302a4a31665cc931da12e8e35157.js
+++ docs/precache-manifest.1751302a4a31665cc931da12e8e35157.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb09f3eab1e0af1574c309e57cac3a66",
+    "revision": "c87c628c6ae932f943f0df3c2d8df4a0",
     "url": "/index.html"
   },
   {
-    "revision": "7686d80d71f1cc582124",
+    "revision": "7c9d37d2f347d8e151be",
     "url": "/static/css/2.d805e148.chunk.css"
   },
   {
-    "revision": "b3c4b2d13a23bf5503ca",
+    "revision": "7b8e9d5993782b33778d",
     "url": "/static/css/main.e541e02e.chunk.css"
   },
   {
-    "revision": "7686d80d71f1cc582124",
-    "url": "/static/js/2.11d2220b.chunk.js"
+    "revision": "7c9d37d2f347d8e151be",
+    "url": "/static/js/2.c345c52f.chunk.js"
   },
   {
     "revision": "0493177a5bc1903429231130df2d56cd",
-    "url": "/static/js/2.11d2220b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c345c52f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b3c4b2d13a23bf5503ca",
-    "url": "/static/js/main.b5713f35.chunk.js"
+    "revision": "7b8e9d5993782b33778d",
+    "url": "/static/js/main.f2d09a6d.chunk.js"
   },
   {
     "revision": "578daf81d163e1be5f0d",
