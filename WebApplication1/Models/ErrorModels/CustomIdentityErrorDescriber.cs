cbe86d0499428de64838edcbf00f04ab24ac5a6a
qalvik <46355180+qalvik@users.noreply.github.com>
2020-08-18 22:39:47 +0200

    Custom Error Messages

--- WebApplication1/Models/ErrorModels/CustomIdentityErrorDescriber.cs
+++ WebApplication1/Models/ErrorModels/CustomIdentityErrorDescriber.cs
@@ -8,202 +8,202 @@ namespace AntilaWebApp.Models.ErrorModels
 {
     public class LocalizedIdentityErrorDescriber : IdentityErrorDescriber
     {
-    //    public override IdentityError DuplicateEmail(string email)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(DuplicateEmail),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.DuplicateEmail, email)
-    //        };
-    //    }
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateEmail),
+                Description = string.Format("Email {0} istnieje już istnieje", email)
+            };
+        }
 
-    //    public override IdentityError DuplicateUserName(string userName)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(DuplicateUserName),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.DuplicateUserName, userName)
-    //        };
-    //    }
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description = string.Format("Nazwa użytkownika {0} jest zajęta", userName)
+            };
+        }
 
-    //    public override IdentityError InvalidEmail(string email)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(InvalidEmail),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.InvalidEmail, email)
-    //        };
-    //    }
+        public override IdentityError InvalidEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidEmail),
+                Description = string.Format("Adres email {0} jest niewłaściwy", email)
+            };
+        }
 
-    //    public override IdentityError DuplicateRoleName(string role)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(DuplicateRoleName),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.DuplicateRoleName, role)
-    //        };
-    //    }
+        public override IdentityError DuplicateRoleName(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateRoleName),
+                Description = string.Format("Nazwa roli {0} jest już zajęta", role)
+            };
+        }
 
-    //    public override IdentityError InvalidRoleName(string role)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(InvalidRoleName),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.InvalidRoleName, role)
-    //        };
-    //    }
+        public override IdentityError InvalidRoleName(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidRoleName),
+                Description = string.Format("Nazwa roli {0} jest nieprawidłowa.", role)
+            };
+        }
 
-    //    public override IdentityError InvalidToken()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(InvalidToken),
-    //            Description = LocalizedIdentityErrorMessages.InvalidToken
-    //        };
-    //    }
+        public override IdentityError InvalidToken()
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidToken),
+                Description = "Nieprawidłowy token."
+            };
+        }
 
-    //    public override IdentityError InvalidUserName(string userName)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(InvalidUserName),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.InvalidUserName, userName)
-    //        };
-    //    }
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidUserName),
+                Description = string.Format("Nazwa użytkownika {0} jest nieprawidłowa, może zawierać jednynie litery lub cyfry.", userName)
+            };
+        }
 
-    //    public override IdentityError LoginAlreadyAssociated()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(LoginAlreadyAssociated),
-    //            Description = LocalizedIdentityErrorMessages.LoginAlreadyAssociated
-    //        };
-    //    }
+        public override IdentityError LoginAlreadyAssociated()
+        {
+            return new IdentityError
+            {
+                Code = nameof(LoginAlreadyAssociated),
+                Description = "Użytkownik z tym loginem już istnieje."
+            };
+        }
 
-    //    public override IdentityError PasswordMismatch()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordMismatch),
-    //            Description = LocalizedIdentityErrorMessages.PasswordMismatch
-    //        };
-    //    }
+        public override IdentityError PasswordMismatch()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordMismatch),
+                Description = "Nieprawidłowe hasło."
+            };
+        }
 
-    //    public override IdentityError PasswordRequiresDigit()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordRequiresDigit),
-    //            Description = LocalizedIdentityErrorMessages.PasswordRequiresDigit
-    //        };
-    //    }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description = "Hasło musi mieć przynajmniej jedną cyfrę ('0' - '9')."
+            };
+        }
 
-    //    public override IdentityError PasswordRequiresLower()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordRequiresLower),
-    //            Description = LocalizedIdentityErrorMessages.PasswordRequiresLower
-    //        };
-    //    }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "Hasło musi mieć przynajmniej jedną małą literę ('A' - 'Z')."
+            };
+        }
 
-    //    public override IdentityError PasswordRequiresNonAlphanumeric()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordRequiresNonAlphanumeric),
-    //            Description = LocalizedIdentityErrorMessages.PasswordRequiresNonAlphanumeric
-    //        };
-    //    }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description = "Hasło musi zawierać minimum jednen znak alfanumeryczny."
+            };
+        }
 
-    //    public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordRequiresUniqueChars),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.PasswordRequiresUniqueChars, uniqueChars)
-    //        };
-    //    }
+        public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUniqueChars),
+                Description = string.Format("Hasło musi mieć minimum {0} unikatowych znaków.", uniqueChars)
+            };
+        }
 
-    //    public override IdentityError PasswordRequiresUpper()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordRequiresUpper),
-    //            Description = LocalizedIdentityErrorMessages.PasswordRequiresUpper
-    //        };
-    //    }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description = "Hasło musi mieć przynajmniej jedną dużą literę ('A' - 'Z')."
+            };
+        }
 
-    //    public override IdentityError PasswordTooShort(int length)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(PasswordTooShort),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.PasswordTooShort, length)
-    //        };
-    //    }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordTooShort),
+                Description = string.Format("Hasło musi mieć minimum {0} znaków.", length)
+            };
+        }
 
-    //    public override IdentityError UserAlreadyHasPassword()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(UserAlreadyHasPassword),
-    //            Description = LocalizedIdentityErrorMessages.UserAlreadyHasPassword
-    //        };
-    //    }
+        public override IdentityError UserAlreadyHasPassword()
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserAlreadyHasPassword),
+                Description = "Użytkownik ma juz hasło."
+            };
+        }
 
-    //    public override IdentityError UserAlreadyInRole(string role)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(UserAlreadyInRole),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.UserAlreadyInRole, role)
-    //        };
-    //    }
+        public override IdentityError UserAlreadyInRole(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserAlreadyInRole),
+                Description = string.Format("Użytkownik jest już w roli {0}.", role)
+            };
+        }
 
-    //    public override IdentityError UserNotInRole(string role)
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(UserNotInRole),
-    //            Description = string.Format(LocalizedIdentityErrorMessages.UserNotInRole, role)
-    //        };
-    //    }
+        public override IdentityError UserNotInRole(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserNotInRole),
+                Description = string.Format("Użytkownik nie jest w roli {0}.", role)
+            };
+        }
 
-    //    public override IdentityError UserLockoutNotEnabled()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(UserLockoutNotEnabled),
-    //            Description = LocalizedIdentityErrorMessages.UserLockoutNotEnabled
-    //        };
-    //    }
+        public override IdentityError UserLockoutNotEnabled()
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserLockoutNotEnabled),
+                Description = "Blokada konta nie jest włączona dla tego użytkownika."
+            };
+        }
 
-    //    public override IdentityError RecoveryCodeRedemptionFailed()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(RecoveryCodeRedemptionFailed),
-    //            Description = LocalizedIdentityErrorMessages.RecoveryCodeRedemptionFailed
-    //        };
-    //    }
+        public override IdentityError RecoveryCodeRedemptionFailed()
+        {
+            return new IdentityError
+            {
+                Code = nameof(RecoveryCodeRedemptionFailed),
+                Description = "Recovery code was not redeemed."
+            };
+        }
 
-    //    public override IdentityError ConcurrencyFailure()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(ConcurrencyFailure),
-    //            Description = LocalizedIdentityErrorMessages.ConcurrencyFailure
-    //        };
-    //    }
+        public override IdentityError ConcurrencyFailure()
+        {
+            return new IdentityError
+            {
+                Code = nameof(ConcurrencyFailure),
+                Description = "Optimistic concurrency failure, object has been modified."
+            };
+        }
 
-    //    public override IdentityError DefaultError()
-    //    {
-    //        return new IdentityError
-    //        {
-    //            Code = nameof(DefaultError),
-    //            Description = LocalizedIdentityErrorMessages.DefaultIdentityError
-    //        };
-    //    }
+        public override IdentityError DefaultError()
+        {
+            return new IdentityError
+            {
+                Code = nameof(DefaultError),
+                Description = "Wystąpił nieznany błąd."
+            };
+        }
     }
 }
